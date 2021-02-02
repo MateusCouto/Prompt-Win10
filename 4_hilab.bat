1f7680dbab2312c57c0462c395caf47664d38545
Mateus Couto <42397879+MateusCouto@users.noreply.github.com>
2021-02-02 16:17:20 -0300

    Add files via upload

--- 4_hilab.bat
+++ 4_hilab.bat
@@ -2,7 +2,7 @@
 
 echo [InternetShortcut] > "%userprofile%\Desktop\Hilab.url"
 echo URL=https://login.hilab.com.br/ >> "%userprofile%\Desktop\Hilab.url"
-echo IconFile=C:\WINDOWS\system32\SHELL32.dll >> "%userprofile%\Desktop\Hilab.url"
-echo IconIndex=20 >> "%userprofile%\Desktop\Hilab.url"
+echo IconFile=C:\Prompt-Win10\favicon.ico >> "%userprofile%\Desktop\Hilab.url"
+echo IconIndex=0 >> "%userprofile%\Desktop\Hilab.url"
 
 cscript api.vbs "Atalho adicionado com Sucesso."
