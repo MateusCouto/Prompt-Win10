6ef39056f392a2f345c0f87ae34c366068c619d8
Mateus Couto <42397879+MateusCouto@users.noreply.github.com>
2021-02-02 16:09:55 -0300

    Update 1_clearlog_cache.bat

--- 1_clearlog_cache.bat
+++ 1_clearlog_cache.bat
@@ -1,42 +1,42 @@
 @echo off
 cls
 
-echo  "Kill Process chrome"
+REM  "Kill Process chrome"
 taskkill /F /IM "chrome.exe" /T
-echo  "Kill Process iexplore"
+REM  "Kill Process iexplore"
 taskkill /F /IM "iexplore.exe" /T
 
-echo "Clear Cache TMP"
+REM "Clear Cache TMP"
 erase "%TEMP%\*.*" /f /s /q
 for /D %%i in ("%TEMP%\*") do RD /S /Q "%%i"
 
-echo "Clear Cache Profile"
+REM "Clear Cache Profile"
 erase "%ALLUSERSPROFILE%\TEMP\*.*" /f /s /q
 for /D %%i in ("%ALLUSERSPROFILE%\TEMP\*") do RD /S /Q "%%i"
 
-echo "Clear Cache SystemRoot"
+REM "Clear Cache SystemRoot"
 erase "%SystemRoot%\TEMP\*.*" /f /s /q
 for /D %%i in ("%SystemRoot%\TEMP\*") do RD /S /Q "%%i"
 erase "%SystemRoot%\TEMP\*.*" /f /s /q
 for /D %%i in ("%SystemRoot%\TEMP\*") do RD /S /Q "%%i"
 
-echo "Clear IE cache -  (Deletes Temporary Internet Files Only)"
+REM "Clear IE cache -  (Deletes Temporary Internet Files Only)"
 RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
 erase "%LOCALAPPDATA%\Microsoft\Windows\Tempor~1\*.*" /f /s /q
 for /D %%i in ("%LOCALAPPDATA%\Microsoft\Windows\Tempor~1\*") do RD /S /Q "%%i"
 
-echo "Clear Google Chrome cache"
+REM "Clear Google Chrome cache"
 erase "%LOCALAPPDATA%\Google\Chrome\User Data\*.*" /f /s /q
 for /D %%i in ("%LOCALAPPDATA%\Google\Chrome\User Data\*") do RD /S /Q "%%i"
 
-echo "Limpar o Cache do Mozilla Firefox"
+REM "Limpar o Cache do Mozilla Firefox"
 erase "%LOCALAPPDATA%\Mozilla\Firefox\Profiles\*.*" /f /s /q
 for /D %%i in ("%LOCALAPPDATA%\Mozilla\Firefox\Profiles\*") do RD /S /Q "%%i"
 
-echo "Clear Cache DNS"
+REM "Clear Cache DNS"
 ipconfig/flushDNS
 
-echo "start Chrome HTTP"
-start chrome https://login.hilab.com.br/
+REM "start Chrome HTTP"
+REM start chrome https://login.hilab.com.br/
 
 cscript api.vbs "Logs do Windows, Google Chrome e Firefox."
